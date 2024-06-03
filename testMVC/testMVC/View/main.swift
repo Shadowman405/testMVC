dbe4af96eebe476323b76a1d80a78d71d30691da
Shadowman405 <maximus405@gmail.com>
2024-06-03 20:01:35 +0300

    update config

--- testMVC/testMVC/View/main.swift
+++ testMVC/testMVC/View/main.swift
@@ -1,25 +1,56 @@
 import Foundation
 
-func run() {
-    let manager = JSONParser.sharedInstance
+//protocol MainViewDelegate {
+//    func update()
+//}
 
-    if let data = jsonMockData {
-        let persons = manager.parseJSON(data: data)
+struct MainView {
+   // var delegate: MainViewDelegate?
 
-        if let persons {
-            let companies = manager.personToCompany(persons: persons)
-            printCompaniesandEmployeesInIt(companies)
-        }
+    func run() {
+//        let manager = JSONParser.sharedInstance
+//    
+//        if let data = jsonMockData {
+//            let persons = manager.parseJSON(data: data)
+//    
+//            if let persons {
+//                let companies = manager.personToCompany(persons: persons)
+//                printCompaniesandEmployeesInIt(companies)
+//            }
+//        }
+//    
+//        func printCompaniesandEmployeesInIt(_ companies: [Company]) {
+//            for company in companies {
+//                print("\n\(company.name.uppercased()):")
+//                for employee in company.employees {
+//                    print("\(employee.fullName); \(company.team); \(employee.email)")
+//                }
+//            }
+//        }
+//
+//        //print("Hello, World! This is my command line tool")
+//
+//        let arguments = CommandLine.arguments
+//        if arguments.count > 1 {
+//            let filePath = arguments[1]
+//            do {
+//                let contents = try String(contentsOfFile: filePath, encoding: .utf8)
+//                print(contents)
+//            } catch {
+//                print("Error reading file")
+//            }
+//        } else {
+//            print("SOmething went wrong")
+//        }
+
     }
 
-    func printCompaniesandEmployeesInIt(_ companies: [Company]) {
-        for company in companies {
-            print("\n\(company.name.uppercased()):")
-            for employee in company.employees {
-                print("\(employee.fullName); \(company.team); \(employee.email)")
-            }
-        }
+    func printEmployees(_ someString: String?) {
+        print(someString ?? "")
     }
 }
 
-run()
+let mainView = MainView()
+let controller = Controller(mainView: mainView)
+//controller.updateMainView()
+
